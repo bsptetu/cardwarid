1f52307bbcceab51a4398ea4f676d4bae1db4e05
bsptetu <55188675+bsptetu@users.noreply.github.com>
2019-09-11 20:09:06 +0900

    sutato20190911

---
+++ js/plugins/kaihen.js
@@ -0,0 +1,8 @@
+﻿(function() {
+var _prevfunc = Game_Action.prototype.executeHpDamage;
+ Game_Action.prototype.executeHpDamage = function(target, value) {
+ _prevfunc.call(this, target, value);
+$gameVariables.setValue(3829,value);
+};
+
+ })();
