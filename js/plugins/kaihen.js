5f007c43aef162bc18c176c42c53ffb770fe116e
bsptetu <55188675+bsptetu@users.noreply.github.com>
2020-02-18 04:52:30 +0900

    20200218

--- js/plugins/kaihen.js
+++ js/plugins/kaihen.js
@@ -172,4 +172,6 @@ Game_Party.prototype.maxBattleMembers = function() {
     return 8;
 };
 
-
+    BattleManager.processActorAuto = function() {
+        this.actor().makeAutoBattleActions();
+    };
